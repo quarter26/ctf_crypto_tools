2f3184099e1d96a34316cab43fb8cef4080ac730
AAAAnda <ad100723@gmail.com>
2020-05-12 10:19:20 +0800

    Update rot13.py

--- rot13.py
+++ rot13.py
@@ -1,4 +1,4 @@
-def rot13(plaintext, remove_punc = False):
+def rot13_1(plaintext, remove_punc = False):
     ciphertext = ""
     if remove_punc == True:
     	plaintext = plaintext.strip(',', '.', ';','!', '?', '~', '=', '+', '-', '_')
@@ -15,6 +15,31 @@ def rot13(plaintext, remove_punc = False):
         ciphertext += chr(tmp)
     return ciphertext
 
+
+def rot13_2(text, isEncrypt):
+    alphabet = u"abcdefghijklmnopqrstuvwxyz"
+    ans = ''
+    key = len(alphabet) >> 1
+    # if number letters in the alphabet is odd
+    if len(alphabet) & 1:
+        alphabet += alphabet[key]
+        key += 1
+    
+    for char in text.lower():
+        try:
+            alphIndex = alphabet.index(char)
+        except ValueError as e:
+            wrchar = char.encode('utf-8')
+            e.args = (
+                "Can't find char '" + wrchar + "' of text in alphabet!",)
+            raise
+        alphIndex = (alphIndex + isEncrypt * key) % len(alphabet)
+        ans += alphabet[alphIndex]
+    return ans
+
+
 plaintext = "ABCabc"
 
-print(rot13(plaintext))
+print(rot13_1(plaintext))
+
+print(rot13_2(plaintext))
